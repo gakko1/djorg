2c32dd88d60e2291b8cf928e119f2cb5ac42aaee
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-09 19:06:51 -0400

    Altered views to add postform

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Bookmark
+from .forms import PostForm
 
-# Create your views here.
+def index(request):
+  context = {}
+  context['bookmarks'] = Bookmark.objects.all()
+  return render(request, 'bookmarks/index.html', context)
+
+def post_new(request):
+  form = PostForm()
+  return render(request, 'bookmarks/postform.html', {'form': form})
