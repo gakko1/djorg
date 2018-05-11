3813420b5a4db37e139f1461a4b310d5a6c777c7
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-11 13:22:35 -0400

    aslkjdf

--- djorg/settings.py
+++ djorg/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '.localhost']
 
 
 # Application definition
