e4ce73994a7e623f62af3ba636b766f71bb32498
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-11 19:27:39 -0400

    'jksad'

--- djorg/settings.py
+++ djorg/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [host.strip() for host in config('ALLOWED_HOSTS').split(',')]
 
 
 # Application definition
