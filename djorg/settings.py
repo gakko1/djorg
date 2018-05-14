1254f7780d2a3df01bd66290d5939b497e1348f6
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-14 17:25:19 -0400

    jklsad

--- djorg/settings.py
+++ djorg/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['.localhost', 'gakko-djorg.herokuapp.com/']
+ALLOWED_HOSTS = ['.localhost', 'gakko-djorg.herokuapp.com']
 
 
 # Application definition
