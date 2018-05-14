00cf082f08a3a0ab0a647b2f39f6fe765f2a9ba3
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-14 17:08:28 -0400

    templates

--- notes/api.py
+++ notes/api.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers, viewsets
 from .models import Note
 
@@ -10,9 +11,24 @@ class NoteSerializer(serializers.HyperlinkedModelSerializer):
     model = Note
     fields = ('title', 'content')
 
+  def create(self, validated_data):
+    """Override create to associate current user with new note."""
+
+    user = self.context['request'].user
+    note = Note.objects.create(user=user, **validated_data)
+    return note
 
 class NoteViewSet(viewsets.ModelViewSet):
   """ViewSet to define the view behavior for Notes."""
 
   serializer_class = NoteSerializer
-  queryset = Note.objects.all()
+  queryset = Note.objects.none()
+
+  def get_queryset(self):
+    user = self.request.user
+    if settings.DEBUG:
+      return Note.objects.all()
+    elif user.is_anonymous:
+      return Note.objects.none()
+    else:
+      return Note.objects.filter(user=user)
