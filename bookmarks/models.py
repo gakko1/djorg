d71ccddf186c33e4d41f166e2d672589235ec544
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-14 17:16:03 -0400

    bookmarks model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Bookmark(models.Model):
-  # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   url = models.URLField('URL', unique=True)
   name = models.CharField(max_length=200)
   notes = models.TextField(blank=True)
