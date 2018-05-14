d951bab410ee28b1aaa0e4d7a4abe55a72f454ee
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-14 18:35:54 -0400

    fixed heroku deployment, removed personal bookmarks

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -10,7 +10,3 @@ class Bookmark(models.Model):
   notes = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
-
-
-class PersonalBookmark(Bookmark):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
